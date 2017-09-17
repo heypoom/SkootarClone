44bc7f0de7e7ea15ffe9aa26ec6f66b9564594aa
Phoomparin Mano <work@phoom.in.th>
2017-09-17 12:51:53 +0700

    :tada: Initialized GitHub Pages

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // This makes it possible to statically export your application,
-  // just in case you don't have the ability to run Node on your production system.
-  exportPathMap: () => ({
-    '/': {page: '/'}
-  })
-}
